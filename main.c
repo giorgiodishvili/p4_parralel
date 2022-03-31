9e84b98a06b66ef45814a479b3179c89f19faed3
giorgiodishvili <godishvili@newage.io>
2022-03-31 10:03:41 +0200

    finished 3rd one

--- main.c
+++ main.c
@@ -34,63 +34,49 @@ int main(int argc, char *argv[]) {
             printf("Enter the number of points: (0 quits) \n");
             scanf("%d", &n);
 
-            for (int i = 1; i < size; i++) {
+            for (int i = 0; i < size; i++) {
                 // Takes buffer, size, type, source, tag, communicator, and status
-                MPI_Send(&n, buffer_len, MPI_INT, i, i, MPI_COMM_WORLD);
+                MPI_Send(&n, 1, MPI_INT, i, i, MPI_COMM_WORLD);
+
             }
-            // Takes buffer, size, type, source, tag, communicator, and status
-            MPI_Recv(buffer, buffer_len, MPI_CHAR, 0, MPI_ANY_TAG,
-                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
 
-            // Print our received message
-            printf("%s\n", buffer);
-        } else {
-            printf("here %d", rank);
+        MPI_Recv(&n, 1, MPI_INT, 0, rank,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-            MPI_Recv(&n, buffer_len, MPI_INT, rank, rank,
-                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("here");
-            if (n == 0) break;
 
-            count = 0;
+        if (n == 0) break;
 
-            for (i = 1; i <= n; i++) {
-                // Get the random numbers between 0 and 1
-                x = ((double) rand()) / ((double) RAND_MAX);
-                y = ((double) rand()) / ((double) RAND_MAX);
+        count = 0;
 
-                // Calculate the square root of the squares
-                z = sqrt((x * x) + (y * y));
+        for (i = 1; i <= n; i += size) {
+            // Get the random numbers between 0 and 1
+            x = ((double) rand()) / ((double) RAND_MAX);
+            y = ((double) rand()) / ((double) RAND_MAX);
 
-                // Check whether z is within the circle
-                if (z <= 1.0)
-                    count++;
+            // Calculate the square root of the squares
+            z = sqrt((x * x) + (y * y));
+
+            // Check whether z is within the circle
+            if (z <= 1.0)
+                count++;
+        }
+        pi = ((double) count / (double) n) * 4.0;
+        sprintf(buffer, "pi is approx. %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
+        MPI_Send(buffer, buffer_len, MPI_CHAR, 0, rank, MPI_COMM_WORLD);
+
+        if (rank == 0) {
+            for (int i = 0; i < size; i++) {
+
+                // Takes buffer, size, type, source, tag, communicator, and status
+                MPI_Recv(buffer, buffer_len, MPI_CHAR, i, MPI_ANY_TAG,
+                         MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                // Print our received message
+                printf("%s\n", buffer);
             }
-            pi = ((double) count / (double) n) * 4.0;
-            sprintf(buffer, "pi is approx. %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
-            MPI_Send(buffer, buffer_len, MPI_CHAR, 0, rank, MPI_COMM_WORLD);
         }
     }
 
     MPI_Finalize();
 
 }
-
-//
-//#include <stdio.h>
-//#include <mpi.h>
-//int main(int argc, char **argv) {
-//    int ierr, num_procs, my_id;
-//
-//    /* find out MY process ID, and how many processes were started. */
-//
-//    ierr = MPI_Init(&argc, &argv);
-//
-//    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
-//    ierr = MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-//
-//    printf("Hello world! I'm process %i out of %i processes\n",
-//           my_id, num_procs);
-//
-//    ierr = MPI_Finalize();
-//}
