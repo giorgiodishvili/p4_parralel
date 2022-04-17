fde5575407cbfcf70b62d0c0696cd3262c629aa0
giorgiodishvili <godishvili@newage.io>
2022-04-17 13:08:11 +0200

    kinda works i guess

--- main2-b.c
+++ main2-b.c
@@ -26,9 +26,57 @@ int MPI_FlattreeColective(const void *sendbuf, void *recvbuf, int count,
     return error;
 }
 
+void MPI_BinomialBcast(void *buffer, int count, MPI_Datatype datatype,
+                       int root, MPI_Comm comm) {
+
+    int size, rank, src, dst;
+
+    MPI_Status status;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size == 1) return;
+
+    int relative_rank = (rank >= root) ? rank - root : rank - root + size;
+
+    int mask = 0x1;
+//    printf("mask %d, size %d, rank %d \n", mask, size, rank);
+    while (mask < size) {
+        if (relative_rank & mask) {
+            src = rank - mask;
+
+            if (src < 0)
+                src += size;
+
+            MPI_Recv(buffer, count, datatype, src, 0, comm, &status);
+
+            break;
+        }
+        mask <<= 1;
+//        printf(" updated mask %d, rank %d \n", mask, rank);
+    }
+
+
+    mask >>= 1;
+
+    while (mask > 0) {
+        if (relative_rank + mask < size) {
+            dst = rank + mask;
+
+            if (dst >= size) {
+                dst -= size;
+            }
+
+            MPI_Send(buffer, count, datatype, dst, 0, comm);
+        }
+        mask >>= 1;
+    }
+}
+
 int main(int argc, char *argv[]) {
 
-    int i, done = 0, n, count;
+    int i, done = 0, n = 0, count;
     double PI25DT = 3.141592653589793238462643;
     double pi, x, y, z;
     // Unique rank is assigned to each process in a communicator
@@ -54,8 +102,7 @@ int main(int argc, char *argv[]) {
             scanf("%d", &n);
         }
 
-        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
+        MPI_BinomialBcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
         if (n == 0) break;
 
         count = 0;
